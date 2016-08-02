88df8b8c55a03fd78ef267cfcd8d4c2393f7fdda
Luke Henke <luop90@gmx.com>
2016-08-02 08:31:16 -0600

    Added skeleton for playback changing

--- assets/src/js/controller.settings.js
+++ assets/src/js/controller.settings.js
@@ -3,12 +3,21 @@ zeus.controller('SettingPageCtrl', ['$scope', '$rootScope', function ($scope, $r
   $scope.settings.darkTheme = !$scope.settings.lightTheme;
 
   $scope.saveSettings = function () {
+    // switch ($('input[type="radio"][name="playbackType"]:checked').attr('id')) {
+    //   case 'stream':
+    //
+    //     break;
+    //   default:
+    //     $scope.settings.playbackType =
+    // }
+
     $rootScope.settings = {
       lightTheme: !$scope.settings.darkTheme,
       animations: $scope.settings.animations,
       analytics: $scope.settings.analytics,
       autoplay: $scope.settings.autoplay,
       volume: $scope.settings.volumeRange
+      // playbackType:
     };
 
     Zeus.saveSettings($rootScope.settings);
